719ba7ddf0e5a94e0142935cad8acbe5abc5116d
Al-Hussien St. Madian <alhussiensm@gmail.com>
2020-03-18 21:11:54 +0200

    test

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace BulkyBook.DataAccess.Data
             : base(options)
         {
         }
+
         public DbSet<CoverType> CoverTypes { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
